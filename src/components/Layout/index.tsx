657556271275249e3316efc9384910f2d94b2ea1
Cosmin Mihai <cmihai@intralinks.com>
2022-05-04 15:01:44 +0300

    create-custom-react-hook-for-animation

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Layout.module.scss';
 import PageContent from './PageContent';
 import PageFooter from './PageFooter';
 import PageHeader from './PageHeader';
+import UseTransitionAnimation from '../../utils/customHooks/useTransitionAnimation';
 
 const Layout: React.FC = () => {
+  const [displayComponent , setDisplayComponent ] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setDisplayComponent(false), 2000);
+  }, []);
+
   return (
     <div className={styles.layout}>
       <PageHeader />
       <PageContent />
-      <PageFooter />
+      <UseTransitionAnimation
+        defaultStyle={{ 
+          display: 'none',
+          transform: 'translateY(100%)',
+          transition: 'transform 500ms ease-in-out'
+        }}
+        duration={500}
+        show={displayComponent}
+        transitionStyle={{
+          entering: {
+            display: 'block',
+            transform: 'translateY(100%)',
+            transition: 'transform 500ms ease-in-out'
+          },
+          entered: {
+            display: 'block',
+            transform: 'translateY(0)',
+            transition: 'transform 500ms ease-in-out'
+          },
+          exiting: {
+            display: 'block',
+            transform: 'translateY(100%)',
+            transition: 'transform 500ms ease-in-out'
+          },
+          exited: {
+            display: 'none',
+            transform: 'translateY(100%)',
+            transition: 'transform 500ms ease-in-out'
+          }
+        }}
+      >
+        <PageFooter />
+      </UseTransitionAnimation>
     </div>
   );
 };
---
+++ src/utils/customHooks/useTransitionAnimation.tsx
@@ -0,0 +1,94 @@
+/*
+  Custom Hook that levereges other Custom React Hooks to give us animation status in real-type and apply the desired transition css style based on these statuses 
+*/
+
+import React, {
+  useState,
+  useEffect
+} from 'react';
+
+type AnimationStatus = 'ENTERING' | 'ENTERED' | 'EXITING' | 'EXITED';
+
+interface PropsFromState {
+  duration: number;
+  defaultStyle: React.CSSProperties;
+  transitionStyle: any;
+  show: boolean;
+  children: React.ReactNode
+}
+
+const UseTransitionAnimation: React.FC<PropsFromState> = props => {
+  const { 
+    duration, 
+    defaultStyle, 
+    transitionStyle, 
+    show,
+    children 
+  } = { ...props};
+
+  const status: {
+    [key in AnimationStatus] : string
+  } = {
+    ENTERING: 'entering',
+    ENTERED: 'entered',
+    EXITING: 'exiting',
+    EXITED: 'exited'
+  };
+
+  //another custom Hook that based on duration, returns both state of type key of AnimationStatus( for example entering, entered, etc.) and it's setState
+  const useTransitionState = (duration: number) => {
+    const [state, setState] = useState(status.EXITED);
+
+    useEffect(() => {
+      let timerId: ReturnType<typeof setTimeout>;
+      if (state === status.ENTERING) {
+        timerId = setTimeout(()=> setState(status.ENTERED), duration);
+      } else if ( state === status.EXITING) {
+        timerId = setTimeout(()=> setState(status.EXITED), duration);
+      }
+
+      return () => {
+        timerId && clearTimeout(timerId);
+      };
+    });
+
+    return [state,  setState] as const;
+  };
+
+  // and another custom Hook that uses the duration and the previous useTransitionState custom hook to output a state of type key of AnimationStatus
+  const useTransitionControl = (duration: number) => {
+    const [state, setState] = useTransitionState(duration);
+    const enter = () => {
+      if (state !==  status.EXITING) {
+        setState(status.ENTERING);
+      }
+    };
+    const exit = () => {
+      if (state !==  status.ENTERING) {
+        setState(status.EXITING);
+      }
+    };
+    return [ state, enter, exit] as const;
+  };
+
+  const [ state, enter, exit ] = useTransitionControl(duration);
+
+  const style = {
+    ...defaultStyle,
+    ...transitionStyle[state] ?? {}
+  };
+
+  useEffect(() => {
+    show ? enter() : exit();
+  },[show]);
+
+  return (
+    <div
+      style={style}
+    >
+      { children }
+    </div>
+  );
+};
+
+export default UseTransitionAnimation;
