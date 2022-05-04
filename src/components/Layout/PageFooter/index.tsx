b4f57256102ef6788ebaee411830a56eb350fd8a
Cosmin Mihai <cmihai@intralinks.com>
2022-05-04 04:57:28 +0300

    arranged footer and general site colours

--- src/components/Layout/PageFooter/index.tsx
+++ src/components/Layout/PageFooter/index.tsx
@@ -1,10 +1,73 @@
 import React from 'react';
 import styles from './PageFooter.module.scss';
+import linkedInLogo from '../../../assets/images/linked-in-logo.png';
+import gitLogo from '../../../assets/images/git-logo.png';
+import twitterLogo from '../../../assets/images/twitter-logo.png';
+import instagramLogo from '../../../assets/images/instagram-logo.png';
+import facebookLogo from '../../../assets/images/facebook-logo.png';
 
 const PageFooter: React.FC = () => {
+const currentYear = new Date().getFullYear();
+
   return (
     <div className={styles['page-footer']}>
-      <h2>footer</h2>
+      <div className={styles.content}>
+        <div className={styles.links}>
+          <a 
+            href="https://linkedin.com/in/cosmin-mihai-62073448"
+            target="new"
+            className={styles.link}
+          > 
+            <img 
+              alt='LinkedIn Icon placeholder'
+              src={linkedInLogo}
+            />
+          </a>
+          <a 
+            href="https://github.com/CosminMihai87" 
+            target="new"
+            className={styles.link}
+          > 
+          <img 
+              alt='Git Icon placeholder'
+              src={gitLogo}
+            />
+          </a>
+          <a 
+            href=""
+            className={styles.link}
+          > 
+          <img 
+              alt='Twitter Icon placeholder'
+              src={twitterLogo}
+            />
+          </a>
+          <a 
+            className={styles.link}
+            href="https://www.instagram.com/cos.min2612" 
+            target="new"
+          > 
+          <img 
+              alt='Instagram Icon placeholder'
+              src={instagramLogo}
+            />
+          </a>
+          <a 
+            className={styles.link}
+            href="https://www.facebook.com/mihai.cosmin.37" 
+            target="new"
+          > 
+          <img 
+              alt='Facebook Icon placeholder'
+              src={facebookLogo}
+            />
+          </a>
+         
+        </div>
+        <span className={styles.signature}>
+          © {currentYear} Cosmin Mihai - <a href="mailto:cosmin.mihai2612@gmail.com">Contact</a>
+        </span>
+      </div>
     </div>
   );
 };
