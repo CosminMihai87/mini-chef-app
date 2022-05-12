1a44ac0a9ca1637cc49ec1f3abc4dd39acacef4f
Cosmin Mihai <cmihai@intralinks.com>
2022-05-12 04:22:27 +0300

    lazy loading comp cretes, also demo mocked dummy component to showcase use

--- src/components/Layout/PageContent/index.tsx
+++ src/components/Layout/PageContent/index.tsx
@@ -1,49 +1,50 @@
-import React from 'react';
+import React, {
+  Suspense,
+  lazy
+} from 'react';
 import styles from './PageContent.module.scss';
 import FwButton from '../../../utils/templates/Button';
-import Loading from '../../Loading';
+import Loading from '../../../components/Loading';
 
 const PageContent: React.FC = () => {
+
+  const DummyComponent = lazy(() => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        return resolve(import('../../../utils/DummyComponent') as any);
+      }, 1500);
+    });
+  });
+
   return (
     <div className={styles['page-content']}>
-      <FwButton
-        animation='progress'
-        onClick={()=>{
-          return ; 
-        }} 
-        variant='primary'
-      >
-        <span>Primary Button</span>
-      </FwButton>
-      <br/> 
-      <FwButton
-        animation='progress'
-        onClick={()=>{
-          return ; 
-        }} 
-        variant='secondary'
-      >
-        <span>Secondary Button</span>
-      </FwButton>
-      <Loading />
-
-      {/* <h1>a</h1>
-      <h1>a</h1>     
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1>
-      <h1>a</h1> */}
-
+      <div className={styles['buttons']}>
+        <FwButton
+          animation='progress'
+          onClick={()=>{
+            return ; 
+          }} 
+          variant='primary'
+        >
+          <span>Primary Button</span>
+        </FwButton>
+        <br/> 
+        <FwButton
+          animation='progress'
+          onClick={()=>{
+            return ; 
+          }} 
+          variant='secondary'
+        >
+          <span>Secondary Button</span>
+        </FwButton> 
+      </div>
+      <br/>
+      <div className={styles['suspense']}>
+        <Suspense fallback={<Loading size={0.7} theme='dark'/>}>
+          <DummyComponent />
+        </Suspense>
+      </div>
 
     </div>
   );
